a824f93b12e286adc04c598f2b6df5b2061fc78f
Feli-Funes-Carcano <feligamer754@gmail.com>
2026-09-01 22:28:22 -0300

    z
    z

---
+++ multy.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+
+    apiKey: "TU_API_KEY",
+
+    authDomain:
+        "TU_PROYECTO.firebaseapp.com",
+
+    databaseURL:
+        "https://TU_PROYECTO-default-rtdb.firebaseio.com",
+
+    projectId:
+        "TU_PROYECTO",
+
+    storageBucket:
+        "TU_PROYECTO.firebasestorage.app",
+
+    messagingSenderId:
+        "TU_ID",
+
+    appId:
+        "TU_APP_ID"
+};
